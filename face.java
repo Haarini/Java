6443b23f4a3e2641536a46cc10900d6f11b66921
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-23 20:46:11 +0530

    Create face.java

---
+++ face.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+class face
+{  
+    public static void main(String args[])
+    {
+       String[] arra = new String[5];
+
+  arra[0] = " +\"\"\"\"\"+ ";
+  arra[1] = "[| o o |]";
+  arra[2] = " |  ^  |";
+  arra[3] = " | '-' |";
+  arra[4] = " +-----+";
+
+  for (int i = 0; i < 5; i++) 
+  {
+   System.out.println(arra[i]);
+  }
+
+ }
+}
+ 
