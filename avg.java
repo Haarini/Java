414a4581067fcb63df045e0f74eeb343047582cc
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-23 20:10:41 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+class Rextester
+{  
+    public static void main(String args[])
+    {
+    int n1,n2,n3,a;
+         
+        System.out.println("Enter three numbers to find average");
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3=sc.nextInt();
+        a=(n1+n2+n3)/3;
+        System.out.println("Average of given numbers is= " + a);
+       
+    }
+}
