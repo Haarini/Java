604474d230c15d0ebce64ffa1a117f392b40f7e5
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 23:22:31 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+//String concatenation
+class concat
+{  
+    public static void main(String args[])
+    {
+  String str1 = "java ";    
+	String str2 = "Exercises"; 
+	
+	System.out.println(str1.substring(1) + str2.substring(1));
+}
+}
+    
