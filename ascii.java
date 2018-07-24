228206cf06e663bf5c8447737a521d9b08704fd3
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 11:03:51 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+
+//printing ascii value
+class ascii
+{  
+ public static void main(String args[])
+ {
+int chr = 'Z';
+		System.out.println("The ASCII value of Z is :"+chr);
+	}
+	}
