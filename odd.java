de23503cabac92a8f93669f7bf77416a1199f512
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 11:22:06 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+
+//To print odd numbers between 1 to 100
+class odd
+{  
+ public static void main(String args[])
+ {
+    for (int i = 1; i < 100; i++)
+    
+    {
+			if (i % 2 != 0)
+            {
+				System.out.println(i);
+			}
+		}
+}
+}
