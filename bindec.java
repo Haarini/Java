017337e526d276798c1f34a2372285a624124ec8
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 10:31:26 +0530

    Rename indec.java to bindec.java

---
+++ bindec.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+//Java program to convert binary to decimal
+class bindec
+{  
+    public static void main(String[] args)
+  {
+  Scanner sc = new Scanner(System.in);
+  long binaryNumber, decimalNumber = 0, j = 1, remainder;
+  System.out.print("Input a binary number: ");
+  binaryNumber = sc.nextLong();
+
+  while (binaryNumber != 0) 
+  {
+   remainder = binaryNumber % 10;
+   decimalNumber = decimalNumber + remainder * j;
+   j = j * 2;
+   binaryNumber = binaryNumber / 10;
+  }
+  System.out.println("Decimal Number: " + decimalNumber);
+ }
+}
+    
