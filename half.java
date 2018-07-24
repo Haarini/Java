f8b9bb8533ba1dd50d4a2b1c0878d4a97fbe7117
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 18:15:22 +0530

    Create half.java

---
+++ half.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+//To print half of a word
+class half
+{  
+    public static void main(String args[])
+    {
+     String main_string = "Implementation";    
+    System.out.println(main_string.substring(0, main_string.length()/2));	
+  } 
+ }
