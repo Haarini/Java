364dad29aff0f3af5953ee4630702f115f6e3ac8
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 18:13:58 +0530

    Create insert.java

---
+++ insert.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+//Inserting a string in middle
+class insert
+{  
+    public static void main(String args[])
+    {
+    String main_string = "Python 3.0";
+   String word = "Tutorial";
+   System.out.println(main_string.substring(0, 7) + word + main_string.substring(6));
+	}
+
+}
+
