a90d56f491133eaf1bd6df874509e24d06f31d46
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 23:25:29 +0530

    Create last3.java

---
+++ last3.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+
+class last3
+{  
+    public static void main(String args[])
+    {
+ String main_string = "Javaide";
+      
+   String last_three_chars = main_string.substring(main_string.length() - 3);
+   System.out.println(last_three_chars + last_three_chars + last_three_chars + last_three_chars);
+}
+}
+    
