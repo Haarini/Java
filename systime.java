f274683ca526c01e7e8cbd1040794b5bf0a0d7a6
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 11:25:01 +0530

    Create systime.java

---
+++ systime.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+//Printing system time
+
+class Rextester
+{  
+ public static void main(String args[])
+ {
+   System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+}
+}
