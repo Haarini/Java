6659e4b6d6050549965b0edff4cddf216ff793a4
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-24 18:12:11 +0530

    Create primesum.java

---
+++ primesum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+//Sum of prime numbers
+class primesum
+{  
+    public static void main(String args[])
+     {
+        int sum = 1;
+	 int ctr = 0; 
+	 int n = 0; 
+    	
+		while (ctr < 100) {
+			n++; 
+			if (n % 2 != 0)
+            { 
+			// check if the number is even
+				if (is_Prime(n))
+                {
+					sum += n; 
+					ctr++; 
+				}
+			}	
+		}
+		System.out.println("\nSum of the first 100 prime numbers: "+sum); 	
+	 }
+	
+   	public static boolean is_Prime(int n)
+    {
+		for (int i = 3; i * i <= n; i+= 2) 
+        {
+			if (n % i == 0) 
+            {
+				return false; 
+			}
+		}
+		return true;
+
+     }
+}
