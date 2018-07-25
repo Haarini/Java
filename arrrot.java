d6d540b372047c7c3f5686e36ef87ad66a9ceb41
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-25 10:03:31 +0530

    Create arrrot.java

---
+++ arrrot.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+//to perform array rotation
+class arrrot
+{  
+    public static void main(String args[])
+   {
+    {
+    int[] array_nums = {78, 12, 900};
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	int[] new_array_nums = {array_nums[1], array_nums[2], array_nums[0]};
+	System.out.println("Rotated Array: "+Arrays.toString(new_array_nums)); 	 
+ }
+}
+}
