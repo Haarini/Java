44d79d1b30607c541fc55879ad70030811b0ed9c
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-25 10:05:22 +0530

    Create largearr.java

---
+++ largearr.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+//To check larger element in array
+class largearr
+{  
+    public static void main(String args[])
+  {
+    int[] array_nums = {23, 30, 10};
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	int max_val = array_nums[0];
+	if(array_nums[2] >= max_val)
+		max_val = array_nums[2];
+	System.out.println("Larger value between first and last element: "+max_val); 	 
+ }
+}
+
