7f3000ed5465f4324b4fa09a80b4f23529b1bebf
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-08-13 22:04:01 +0530

    Create min_conv.java

---
+++ min_conv.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+//Converting minutes to years and days
+import java.util.Scanner;
+class min_conv
+{  
+ 
+
+
+    public static void main(String[] Strings) {
+
+
+        double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    }
+}
