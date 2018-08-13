dbee52741eead5c36a0fa96cb82d0b50301a61c8
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-08-13 21:58:58 +0530

    Create inch_meter.java

---
+++ inch_meter.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+//converting inch to meters
+import java.util.Scanner;
+class inch_meter
+{
+public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a value for inch: ");
+        double inch = input.nextDouble();
+        double meters = inch * 0.0254;
+        System.out.println(inch + " inch is " + meters + " meters");
+
+    }
+}
